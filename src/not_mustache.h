554c4a9cc0b23207de739064134e65998276658b
Tripp R <60992655+RealTrippR@users.noreply.github.com>
2025-11-14 16:43:17 -0500

    fixed typo in LICENSE

--- src/not_mustache.h
+++ src/not_mustache.h
@@ -1,6 +1,7 @@
 /*
 -+- -+- -+- -+- -+- -+- -+- -+- -+- -+- -+- -+- -+- -+- -+- -+- -+- -+- 
 
+
 Robins Free of Charge & Open Source Public License 25
 
 Copyright (C), 2025 - Tripp R. All rights reserved.
@@ -10,14 +11,14 @@ shall hereby be granted, free of charge, to be used for any purpose, including c
 modification, merging, and redistrubution. The software is provided 'as-is' and comes without any
 express or implied warranty. This license is valid under the following restrictions:
 
-1. The origin of the software must not be misrepresentented; only the true author(s) of the software
-must be attributed as the it's creators. This applies every alteration of the "software", the name(s)
-of the developer(s) of any alterations must be appended to the list of names of
+1. The origin of the software must not be misrepresentented; the true author(s) of the software
+must be attributed as such. This applies every alteration of the "software", the name(s)
+of the authors(s) of any alterations must be appended to the list of names of
 the author(s) of the preceding version of the software which the alteration is based upon.
 
 2. This license must be included in all redistributions of the software source.
 
-3. All distrubitions of altered forms of the software must be clearly marked as such.
+3. All distributions of altered forms of the software must be clearly marked as such.
 
 4. The author(s) of this software and all subsequent alterations hold no responsibility for any
 damages that may result from use of the software.
